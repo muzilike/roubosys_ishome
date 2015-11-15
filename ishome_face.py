fd6ad059c68f9b3e8e09c00d959f91db10522395
JunJian <junjian@mochui.net>
2015-11-16 00:07:10 +0800

    [face]:fix

--- ishome_face.py
+++ ishome_face.py
@@ -2,14 +2,16 @@
 # encoding: utf-8
 
 import cv2.cv as cv
+import cv2
 capture=cv.CaptureFromCAM(0)
-hc = cv.Load("/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml")
+#hc = cv.Load("/usr/local/share/OpenCV/haarcascades/haarcascade_frontalface_alt.xml")
+cascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_alt.xml")
 while True:
-  frame=cv.QueryFrame(capture)
-  faces = cv.HaarDetectObjects(frame, hc, cv.CreateMemStorage(), 1.2, 2, cv.CV_HAAR_DO_CANNY_PRUNING, (0, 0))
+  frame = cv.QueryFrame(capture)
+  faces = cascade.detectMultiScale(frame)
   for ((x, y, w, h), stub) in faces:
     cv.Rectangle(frame, (int(x), int(y)), (int(x)+w, int(y)+h), (0, 255, 0), 2, 0)
-    cv.ShowImage("Window", frame)
+  cv.ShowImage("Window", frame)
   c=cv.WaitKey(1)
   if c==27 or c == 1048603:
     break
