f06da77167cb1c08c8106ee8bdc614bcd1d2c496
JunJian <junjian@mochui.net>
2015-11-16 00:34:43 +0800

    [face]: fix big

--- ishome_face.py
+++ ishome_face.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import cv2
+import cv2.cv as cv
 
 capture=cv2.VideoCapture(0)
-print capture.isOpened()
+cascade_fn = 'haarcascades/haarcascade_frontalface_alt.xml'
+cascade = cv2.CascadeClassifier(cascade_fn)
 while True:
   ret, img=capture.read()
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  gray = cv2.equalizeHist(gray)
+  rects = cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5, minSize=(30, 30), flags = cv.CV_HAAR_SCALE_IMAGE)
+  for x1, y1, x2, y2 in rects:
+    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
   cv2.imshow('Video', img)
   key=cv2.waitKey(1)
   if key==ord('q'):
