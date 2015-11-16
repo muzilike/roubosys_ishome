d3d6d7539c81614c588d8f125a5afc14e8d52d57
JunJian <junjian@mochui.net>
2015-11-16 21:07:10 +0800

    [video]: fix after test

--- ishome_video.py
+++ ishome_video.py
@@ -18,19 +18,17 @@ while True:
     firstFrame = gray_step2
     continue
 
-  firstFrame = gray_step2.copy()
   frameDelta = cv2.absdiff(firstFrame, gray_step2)
   thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
   thresh = cv2.dilate(thresh, None, iterations=2)
   (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_CCOMP, cv2.CHAIN_APPROX_SIMPLE)
+  firstFrame = gray_step2.copy()
   if len(cnts) == 0:
     continue
-
   gray_step3 = cv2.equalizeHist(gray_step1)
   rects = cascade.detectMultiScale(gray_step3, scaleFactor=1.2, minNeighbors=5)
   if len(rects) == 0:
     continue
-
   for x1, y1, x2, y2 in rects:
     cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
   cv2.imshow("Security Feed", frame)
